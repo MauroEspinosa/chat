0ca8ed35395453f407fcf6c92c1fb55777df32f7
MauroEspinosa <maauurooxd@hotmail.com>
2016-12-31 13:45:23 -0600

    0.1

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ var path = require ('path');
 
 
 
-app.set('views', path.join(__dirname +'../views'));
-app.use(express.static(path.join(__dirname + '../public')));
+app.set('views', path.join(__dirname +'/../views'));
+app.use(express.static(path.join(__dirname + '/../public')));
 app.set("view engine", "ejs");
 
 
 app.get("/", function(req,res){
-  res.render("index");
+  res.render("index.ejs");
 });
 
 io.on("connection", function(socket){
