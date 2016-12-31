525f878baff59e1fed8aca9933de2091ef9822c1
MauroEspinosa <maauurooxd@hotmail.com>
2016-12-31 13:23:37 -0600

    0.1

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var path = require ('path');
 
 
 app.set('views', path.join(__dirname +'/../views'));
-app.use(express.static(path.join(__dirname + '/public')));
+app.use(express.static(path.join(__dirname + '/../public')));
 app.set("view engine", "ejs");
 
 
